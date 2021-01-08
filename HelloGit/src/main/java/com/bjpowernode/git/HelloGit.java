36792b8ab1e43e7fb6f4d532a8ff8af7dbe6e24c
zhangsan_pro <764291080@qq.com>
2021-01-08 09:46:49 +0800

    创建分支

--- HelloGit/src/main/java/com/bjpowernode/git/HelloGit.java
+++ HelloGit/src/main/java/com/bjpowernode/git/HelloGit.java
@@ -3,5 +3,7 @@ package com.bjpowernode.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
